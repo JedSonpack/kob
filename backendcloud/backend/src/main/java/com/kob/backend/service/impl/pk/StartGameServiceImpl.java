dccd52686aafc4b676e314196d2d4bab0e6ce481
Heng Li <2114654816@qq.com>
2023-05-29 16:44:32 +0800

    实现了微服务的继承，能够用两个微服务来进行匹配操作，将rating移到User中

---
+++ backendcloud/backend/src/main/java/com/kob/backend/service/impl/pk/StartGameServiceImpl.java
@@ -0,0 +1,17 @@
+package com.kob.backend.service.impl.pk;
+
+import com.kob.backend.service.pk.StartGameService;
+import com.kob.backend.websocket.WebSocketServer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StartGameServiceImpl implements StartGameService {
+
+    @Override
+    public String startGame(Integer aId, Integer bId) {
+        //a 和 b 可以开始游戏了
+        System.out.println("现在:" + aId + " " + bId + "可以进行游戏");
+        WebSocketServer.startGame(aId, bId);
+        return "success Satrt!";
+    }
+}
---
+++ backendcloud/backend/src/main/java/com/kob/backend/websocket/WebSocketServer.java
@@ -0,0 +1,197 @@
+package com.kob.backend.websocket;
+
+import com.alibaba.fastjson.JSONObject;
+import com.kob.backend.mapper.RecordMapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.Record;
+import com.kob.backend.pojo.User;
+import com.kob.backend.websocket.utils.Game;
+import com.kob.backend.websocket.utils.JwtAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+@ServerEndpoint("/websocket/{token}")  // 注意不要以'/'结尾
+public class WebSocketServer {
+
+    // 后端向前端发送消息
+    private Session session = null;
+
+    // 在本类中维护一个静态表，将用户id和websocket对应起来，不需要初始化实例就可以有这个表  对所有实例都可见且只能在本类中使用
+    final public static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+
+    // 这个连接用的是谁(用户)
+    private User user;
+
+    // 需要注入usermapper 单是由于是多线程，所以注入有区别
+    private static UserMapper userMapper;
+
+    // 存储游戏对象
+    private Game game = null;
+
+    //路径
+    private final static String addPlayerUrl = "http://127.0.0.1:3001/player/add/";
+    private final static String removePlayerurl = "http://127.0.0.1:3001/player/remove/";
+
+
+    public static RecordMapper recordMapper;
+    private static RestTemplate restTemplate;
+
+    @Autowired
+    public void setRecordMapper(RecordMapper recordMapper) {
+        WebSocketServer.recordMapper = recordMapper;
+    }
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper) {
+        WebSocketServer.userMapper = userMapper;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        WebSocketServer.restTemplate = restTemplate;
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) throws IOException {
+        // 建立连接
+        // 保存以下session
+        this.session = session;
+        System.out.println("Connected!");
+
+        int userId = JwtAuthentication.getUserId(token);
+
+        this.user = userMapper.selectById(userId);
+
+        if (this.user != null) {
+            users.put(userId, this);
+        } else {
+            this.session.close();
+        }
+
+
+        System.out.println(users);
+
+    }
+
+    @OnClose
+    public void onClose() {
+        System.out.println("disconnected!");
+        if (this.user != null) {
+            users.remove(this.user.getId());
+        }
+    }
+
+    public static void startGame(Integer aId, Integer bId) {
+        User a = userMapper.selectById(aId), b = userMapper.selectById(bId);
+        Game game = new Game(13, 14, 20, a.getId(), b.getId());
+        game.createMap();
+        if (users.get(a.getId()) != null)
+            users.get(a.getId()).game = game;
+        if (users.get(b.getId()) != null)
+            users.get(b.getId()).game = game;
+
+        game.start();
+
+        JSONObject respGame = new JSONObject();
+        respGame.put("a_id", game.getPlayerA().getId());
+        respGame.put("a_sx", game.getPlayerA().getSx());
+        respGame.put("a_sy", game.getPlayerA().getSy());
+        respGame.put("b_id", game.getPlayerB().getId());
+        respGame.put("b_sx", game.getPlayerB().getSx());
+        respGame.put("b_sy", game.getPlayerB().getSy());
+        respGame.put("map", game.getG());
+
+        JSONObject respA = new JSONObject();
+        respA.put("event", "start-matching");
+        respA.put("opponent_username", b.getUsername());
+        respA.put("opponent_photo", b.getPhoto());
+        respA.put("game", respGame);
+        if (users.get(a.getId()) != null)
+            users.get(a.getId()).sendMessage(respA.toJSONString());
+
+        JSONObject respB = new JSONObject();
+        respB.put("event", "start-matching");
+        respB.put("opponent_username", a.getUsername());
+        respB.put("opponent_photo", a.getPhoto());
+        respB.put("game", respGame);
+        if (users.get(b.getId()) != null)
+            users.get(b.getId()).sendMessage(respB.toJSONString());
+    }
+
+
+    private void startMatching() {
+
+        System.out.println("start matching!");
+        //像后端发请求
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+
+        data.add("user_id", this.user.getId().toString());
+
+        data.add("rating", this.user.getRating().toString());
+
+
+        restTemplate.postForObject(addPlayerUrl, data, String.class); //返回值的class
+
+    }
+
+    private void stopMatching() {
+        System.out.println("stop matching");
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("user_id", this.user.getId().toString());
+        restTemplate.postForObject(removePlayerurl, data, String.class);
+
+    }
+
+    private void move(int d) { //收到消息后进行移动
+        if (game.getPlayerA().getId().equals(user.getId())) {
+            game.setNextStepA(d);
+        } else if (game.getPlayerB().getId().equals(user.getId())) {
+            game.setNextStepB(d);
+        }
+    }
+
+
+    @OnMessage
+    public void onMessage(String message, Session session) { //当作路由 判断把任务交给谁
+        // 从Client接收消息
+        System.out.println("receive message!");
+        JSONObject data = JSONObject.parseObject(message);
+        String event = data.getString("event");
+        if ("start-matching".equals(event)) {
+            startMatching();
+        } else if ("stop-matching".equals(event)) {
+            stopMatching();
+        } else if ("move".equals(event)) {
+            move(data.getInteger("direction"));
+        }
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    // 辅助函数 后端向前端发送消息
+    public void sendMessage(String text) {
+        synchronized (this.session) {
+            try {
+                this.session.getBasicRemote().sendText(text);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
